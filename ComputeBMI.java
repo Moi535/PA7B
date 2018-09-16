39f7079f1c5548b03bdc1c697deb388347ad1430
Moi535 <moymontes961347@gmail.com>
2018-09-16 18:04:25 -0500

    Nuevo

---
+++ ComputeBMI.java
@@ -0,0 +1,9 @@
+public class ComputeBMI{
+  public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+    final double KILOGRAMS_PER_POUND = 0.4535923;
+    final double METERS_PER_INCH = 0.0254;
+  }
+}
+
+}
